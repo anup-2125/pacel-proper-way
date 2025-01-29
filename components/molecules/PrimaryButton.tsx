3d6aab1f5e615670b8544629d1bcae3cd1a72b05
anup-2125 <anup.narvekar@kilowott.com>
2025-01-29 16:55:21 +0530

    Added atomic components

---
+++ components/molecules/PrimaryButton.tsx
@@ -0,0 +1,17 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import StyledButton from '@/components/atoms/StyledButton'
+import colors from '@/constants/colours'
+
+
+interface PrimaryButtonProp {
+  text: string
+}
+
+const PrimaryButton: React.FC<PrimaryButtonProp> = ({text}) => {
+  return (
+    <StyledButton text={text} textTransform='capitalize' bgColor={colors.secondary} textColor='#fff' fontSize='16'/>
+  )
+}
+
+export default PrimaryButton

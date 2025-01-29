4a4a857733013d00f85bef750902751d16fbb4da
anup-2125 <anup.narvekar@kilowott.com>
2025-01-29 17:46:48 +0530

    Added card component

--- components/cards/ParcelCard.tsx
+++ components/cards/ParcelCard.tsx
@@ -40,6 +40,20 @@ const UpperSection = styled.View<cardContainerProp>`
 const ParcelCard = () => {
   return (
     <CardContainer>
+        {/* <UpperSection>
+            <Image source={{uri:'https://images.pexels.com/photos/1666070/pexels-photo-1666070.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'}}/>
+            <View>
+
+            </View>
+        </UpperSection>
+        <MidSection>
+
+        </MidSection>
+        <BottomSection>
+            
+        </BottomSection> */}
+
+        
 
     </CardContainer>
   )

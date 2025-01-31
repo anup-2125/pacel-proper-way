bf4e5cb3722682466b4ae4d17498f51728534b16
anup-2125 <anup.narvekar@kilowott.com>
2025-01-31 16:13:32 +0530

    Added mapview

--- app/index.tsx
+++ app/index.tsx
@@ -3,27 +3,30 @@ import styled from "styled-components/native";
 import ParcelCard from "@/components/orgamisms/cards/ParcelCard";
 import colors from "@/constants/colours";
 import React, { useRef } from "react";
+import BottomDrawer from "@/components/orgamisms/cards/BottomDrawer";
+import BtmDrwr from "@/components/orgamisms/cards/BtmDrwr";
+import MapView from 'react-native-maps';
+
 
 const ScreenContainer = styled.SafeAreaView`
   height: 100%;
   align-items: center;
   justify-content: center;
   background-color: darkgray;
-  padding: 16px;
 `
 
 const StyledScrollView = styled.ScrollView`
   flex: 1;
   padding-top: 16px;
+  z-index: 10;
 `;
 
+const PaddedView = styled.View`
+  width: 96%;
+  height: 100%;
+`
 
 export default function Index() {
-
-  const modalizeRef = useRef(null);
-
-  // const bottomSheetRef = useRef<BottomSheet>(null);
-  const snapPoints = ["25%", "50%", "90%"];
 
   return (
     <ScreenContainer>
@@ -32,15 +35,32 @@ export default function Index() {
         backgroundColor={colors.primary}
       />
 
-      <StyledScrollView showsVerticalScrollIndicator={false}>
+      <MapView style={{ width: '100%', height: '100%' }}
+        initialRegion={{
+          latitude: 15.6142,
+          longitude: 73.7847,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }} showsUserLocation={true} />
 
-        <ParcelCard />
-        <ParcelCard />
-        <ParcelCard />
-        <ParcelCard />
-        <ParcelCard />
+      <PaddedView>
 
-      </StyledScrollView>
+        <StyledScrollView showsVerticalScrollIndicator={false}>
+
+          <ParcelCard />
+          <ParcelCard />
+          <ParcelCard />
+          <ParcelCard />
+          <ParcelCard />
+
+        </StyledScrollView>
+
+      </PaddedView>
+
+
+
+      {/* <BottomDrawer /> */}
+
 
 
     </ScreenContainer>
--- components/orgamisms/cards/ParcelCard.tsx
+++ components/orgamisms/cards/ParcelCard.tsx
@@ -47,7 +47,7 @@ const UpperSection = styled.View<cardContainerProp>`
 
 const ParcelCardData: ParcelDetailsProp = {
     cardId: '123',
-    due: '12-12-2025 12:24 PM',
+    due: '12-12-2025',
     imageURL: 'https://images.pexels.com/photos/1666070/pexels-photo-1666070.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
     recipientName: 'Anup Narvekar',
     eta: '30mins'
